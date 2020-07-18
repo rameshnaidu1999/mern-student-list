936e1d52a6fc3de560b1d82d71e561293fd8be76
rameshnaidu1999 <rameshnaidu1999@gmail.com>
2020-07-18 21:24:26 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -30,17 +30,8 @@ app.use('/students', studentRoute)
 
 // PORT
 const port = process.env.PORT || 4000;
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log('Connected to port ' + port)
 })
 
-// 404 Error
-app.use((req, res, next) => {
-  next(createError(404));
-});
 
-app.use(function (err, req, res, next) {
-  console.error(err.message);
-  if (!err.statusCode) err.statusCode = 500;
-  res.status(err.statusCode).send(err.message);
-});
